14ecbe8ea8d3495f334608dca4b57c9f20ab6468
Manoj Kumar Sharma <mailme@manojkumarsharma.com>
2022-03-01 12:22:32 +0530

    Scaffolded the Identity UI pages to address the customization needs of the Identity User and Identity Role types.

---
+++ GuniKitchen.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using GuniKitchen.Web.Data;
+using GuniKitchen.Web.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(GuniKitchen.Web.Areas.Identity.IdentityHostingStartup))]
+namespace GuniKitchen.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
